9d2e3535e6fda18b94377c40ccda4612e5fc3180
minaton-ru <minaton@yandex.ru>
2023-10-06 08:48:04 +0300

    default active order fix

--- orders/models.py
+++ orders/models.py
@@ -7,7 +7,7 @@ class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     robot_serial = models.CharField(max_length=5, blank=False, null=False)
     created = models.DateTimeField(auto_now_add=True)
-    active = models.BooleanField(default=False)
+    active = models.BooleanField(default=True)
 
     def __str__(self) -> str:
-        return f'{self.robot_serial} ({self.pk})'
+        return f'{self.pk} ({self.robot_serial})'
